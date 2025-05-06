9180b8894a3f57c1b3f94db23c713518112a2528
Juan2400 <juanfebrecarrion145@gmail>
2025-05-06 16:43:41 -0500

    se agrego el views.py y el template

--- app_productos/urls.py
+++ app_productos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.produtos, name='Productos'),
+    path('', views.productos, name='Productos'),
 ]
--- app_productos/views.py
+++ app_productos/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Producto, Categoria
 
-# Create your views here.
-def produtos(request):
-    pass
+def productos(request):
+    # Obtener todas las categorías
+    categorias = Categoria.objects.all()
+    
+    # Obtener todos los productos activos con sus categorías
+    productos = Producto.objects.filter(activo=True).select_related('categoria')
+    
+    return render(request, 'app_productos/productos.html', {
+        'productos': productos,
+        'categorias': categorias,
+    })
