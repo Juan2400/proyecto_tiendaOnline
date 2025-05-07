05172cdba0a9cc1050faffa6b2ade0d7f19e9af5
Juan2400 <juanfebrecarrion145@gmail>
2025-05-07 16:48:37 -0500

    se agrego la url, vista y template

--- app_nosotros/urls.py
+++ app_nosotros/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    
+    path('', views.nosotros, name='Nosotros'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_nosotros/views.py
+++ app_nosotros/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def nosotros(request):
+    return render(request, 'app_nosotros/nosotros.html')
